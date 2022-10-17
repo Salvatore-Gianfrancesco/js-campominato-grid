672cb484fc37457fb21d89a13cccb9f4ee9e39d9
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-17 14:20:06 +0200

    added grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-alert()
+const containerEl = document.querySelector(".container");
+
+const maxValue = 100;
+
+/* Grid creation */
+for (let i = 0; i < maxValue; i++) {
+    const cellEl = document.createElement("div");
+    cellEl.classList.add("cell");
+    cellEl.innerText = i + 1;
+
+    containerEl.insertAdjacentElement("beforeend", cellEl);
+    console.log(cellEl);
+}
