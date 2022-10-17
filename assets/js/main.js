93c70a019f276423d994eea3019b5c53129cb63f
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-17 14:38:55 +0200

    added event toggler

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ for (let i = 0; i < maxValue; i++) {
     cellEl.innerText = i + 1;
 
     containerEl.insertAdjacentElement("beforeend", cellEl);
-    console.log(cellEl);
+    // console.log(cellEl);
 }
 
 /* Button that generates the grid */
@@ -18,3 +18,16 @@ buttonEl.addEventListener("click", function () {
     containerEl.classList.remove("d-none");
     buttonEl.classList.add("d-none");
 });
+
+const cellList = document.getElementsByClassName("cell");
+// console.log(cellList);
+// console.log(cellList[3]);
+
+/* Background toggler */
+for (let i = 0; i < cellList.length; i++) {
+    const cellEl = cellList[i];
+    cellEl.addEventListener("click", function () {
+        cellEl.classList.toggle("light_blue");
+        console.log(i + 1);
+    });
+}
